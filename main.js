a252f0bd7f1ce5eb87c102039c96a43ca62375dd
Otkir77 <166150681+Otkir77@users.noreply.github.com>
2024-07-06 15:06:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let word = prompt('Istalgan sozni kiriting')
+
+let color = prompt('Rang kiriting')
+
+let bgcolor = prompt ("Bg rangini kiriting")
+
+let body = document. querySelector ('body')
+
+let str = document. createElement('p')
+
+str.innerHTML = word
+
+body.append(str)
+
+str.style.color = color
+
+body.style.background = bgcolor
+
+str.style.textAlign = 'center'
+str.style.fontSize = '45px'
+str.style.marginTop = '350px'
+
+
+str.classList.add('word')
